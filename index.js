93a598219bf06dc886856cd514a70731af4773c8
Brandon Weaver <a7xncob@gmail.com>
2020-02-23 19:27:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
 {
   return Object.assign(driver, { [key]: value });
 }
+
+function deleteFromDriverByKey(driver, key)
+{
+  delete driver.key;
+}
